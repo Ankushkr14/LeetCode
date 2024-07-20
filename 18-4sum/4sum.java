88ddcabec7da656fc8d6633d8df088761ca771ec
Ankush Kumar <91725844+Ankushkr14@users.noreply.github.com>
2024-07-21 01:57:36 +0530

    Time: 16 ms (81.96%) | Memory: 43.7 MB (79.27%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -2,37 +2,45 @@ class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         Arrays.sort(nums);
         int n = nums.length;
-        if(nums==null || n<4){
-            return new ArrayList<>();
+
+        List<List<Integer>> list = new ArrayList<>();
+        if(n<4){
+            return list;
         }
-        
-        HashSet<List<Integer>> list = new HashSet<>();
-      
         
 
         for(int i=0;i<n-3;i++){
-
+            if(i>0 && nums[i]==nums[i-1])
+                continue;
             for(int j=i+1;j<n-2;j++){
-
+                if(j>i+1 && nums[j]==nums[j-1])
+                    continue;
 
                 int k = j+1;
                 int l = n-1;
 
                 while(k<l){
-                    long sum = (long)nums[i]+  (long)nums[j]+  (long)nums[k]+  (long)nums[l];
-                    if(sum == target){
-                        List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k],nums[l]);
+                    long sum = (long)nums[i]+(long)nums[j]+(long)nums[k]+(long)nums[l];
+                    if(sum<target)
+                        k++;
+
+                    else if(sum>target)
+                        l--;
+                    else{
+                            List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k],nums[l]);
                             list.add(temp);
                             k++;
                             l--;
-                    }else if(sum>target)
-                            l--;
-                    else
-                        k++;
+                            
+                            while(k<l && nums[k]==nums[k-1])
+                                k++;
+                            while(k<l && nums[l]==nums[l+1])
+                                l--;
+                        }
                 }
             }
 
         }
-        return new ArrayList<>(list);
+        return list;
     }
 }
