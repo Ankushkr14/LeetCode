e1fe71ab28308b9bdfe818e3240d7f10a05a0aec
Ankush Kumar <91725844+Ankushkr14@users.noreply.github.com>
2024-08-20 08:31:04 +0530

    Time: 9 ms (92.23%) | Memory: 57.4 MB (9.56%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -1,34 +1,61 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+
 class Solution {
-    public ListNode sortList(ListNode head) {
-        ArrayList<Integer> list = new ArrayList<>();
-        ListNode temp = head;
-
-        while(temp != null){
-            list.add(temp.val);
-            temp = temp.next;
+    private ListNode findMid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        fast = fast.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
+        ListNode mid=slow.next;
+        slow.next=null;
+        return mid;
+    }
 
-        Collections.sort(list);
-        temp = head;
-        int i =0;
-        while(temp!= null){
-            if(i<list.size()){
-                temp.val = list.get(i);
-                i++;
+    private ListNode merge(ListNode leftHead, ListNode rightHead) {
+        ListNode newHead = new ListNode(0); // Dummy node to help with merging
+        ListNode current = newHead;
+
+        while (leftHead != null && rightHead != null) {
+            if (leftHead.val < rightHead.val) {
+                current.next = leftHead;
+                leftHead = leftHead.next;
+            } else {
+                current.next = rightHead;
+                rightHead = rightHead.next;
             }
-            temp = temp.next;
+            current = current.next;
+        }
+        if(leftHead!=null){
+            current.next=leftHead;
+        }
+        if(rightHead!=null){
+            current.next=rightHead;
+        }
+        return newHead.next;
+    }
+
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
         }
 
-        return head;
+        ListNode mid=findMid(head);
+        ListNode left=sortList(head);
+        ListNode right=sortList(mid);
+
+        return merge(left, right);
     }
 }
