4168aa8039d89cc82d137d1c764c6f54a491e7ca
Ankush Kumar <91725844+Ankushkr14@users.noreply.github.com>
2025-01-02 10:56:47 +0530

    Time: 9 ms (35.76%) | Memory: 85.7 MB (63.33%) - LeetSync

--- 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.java
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.java
@@ -1,29 +1,28 @@
 class Solution {
     public int[] vowelStrings(String[] words, int[][] queries) {
-        int[] ans = new int[queries.length];
+        int n = queries.length;
+        int m = words.length;
+
+        int[] result = new int[n];
 
         Set<Character> vowels = new HashSet<>(Arrays.asList('a','e','i','o','u'));
+        int[] sum = new int[m+1];
 
-        int[] prefixSum = new int[words.length+1];
-        int n = words.length;
-
-        for(int i =0 ; i<n; i++){
+        for(int i =0;i<m;i++){
             String curr = words[i];
-            if(vowels.contains(curr.charAt(0)) && vowels.contains(curr.charAt(curr.length() - 1))){
-                prefixSum[i+1]= 1+prefixSum[i];
+
+            if(vowels.contains(curr.charAt(0))&& vowels.contains(curr.charAt(curr.length()-1))){
+                sum[i+1] = 1 + sum[i];
             }else{
-                prefixSum[i+1]= prefixSum[i];
+                sum[i+1] = sum[i]; 
             }
         }
 
-        for(int i =0;i<queries.length;i++){
+        for(int i=0;i<n;i++){
             int start = queries[i][0];
             int end = queries[i][1];
-
-            ans[i]= prefixSum[end+1]-prefixSum[start];
+            result[i] = sum[end+1] -sum[start];
         }
-
-        return ans;
-        
+        return result;
     }
 }
